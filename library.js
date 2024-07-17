90756d43afad2131a43317dfe97b29630424557b
Vera Childs <141092487+boscopup@users.noreply.github.com>
2024-07-16 20:31:45 -0500

    Add modal dialog and add book data to library

--- library.js
+++ library.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(title, author, pages, haveRead) {
     this.title = title;
     this.author = author;
@@ -15,5 +17,33 @@ function Book(title, author, pages, haveRead) {
     };
 }
 
-theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
-theHobbit.info();
+function addBookToLibrary() {
+    let title, author, pages, haveRead;
+    title = document.querySelector('#title').value;
+    author = document.querySelector('#author').value;
+    pages = document.querySelector('#pages').value;
+    haveRead = document.querySelector('#haveRead').checked;
+    book = new Book(title, author, pages, haveRead);
+    myLibrary.push(book);
+}
+
+const dialog = document.querySelector("dialog");
+const saveButton = document.querySelector("dialog button#save");
+const cancelButton = document.querySelector("dialog button#cancel");
+
+const addBookButton = document.querySelector("#addBook");
+
+cancelButton.addEventListener("click", () => {
+    dialog.close();
+    console.log("Closed");
+});
+
+saveButton.addEventListener("click", () => {
+    dialog.close();
+    addBookToLibrary();
+    console.log("Saved\n");
+} );
+
+addBookButton.addEventListener("click", () => {
+    dialog.showModal();
+});
