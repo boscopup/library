c12d3df9b565d22a7a51f18405e5baf28c4d1469
Vera Childs <141092487+boscopup@users.noreply.github.com>
2024-07-17 10:48:04 -0500

    Display books added to library
    This currently has no styling.

--- library.js
+++ library.js
@@ -27,6 +27,44 @@ function addBookToLibrary() {
     myLibrary.push(book);
 }
 
+function refreshBookDisplay() {
+    let bookDiv, p;
+    let display = document.querySelector("#bookDisplay");
+    display.replaceChildren();  // Empty what's there
+    myLibrary.forEach(book => {
+        bookDiv = document.createElement("div")
+        bookDiv.classList.add("book");
+        p = document.createElement("p");
+        p.classList.add("bookTitle");
+        p.textContent = book.title;
+        bookDiv.appendChild(p);
+        p = document.createElement("p");
+        p.classList.add("bookAuthor");
+        p.textContent = book.author;
+        bookDiv.appendChild(p);
+        p = document.createElement("p");
+        p.classList.add("bookPages");
+        p.textContent = book.pages + " pages";
+        bookDiv.appendChild(p);
+        p = document.createElement("p");
+        p.classList.add("bookReadStatus");
+        if (book.haveRead) {
+            p.textContent = "Have read";
+        } else {
+            p.textContent = "Have not read";
+        }
+        bookDiv.appendChild(p);
+        display.appendChild(bookDiv);
+    });
+}
+
+function clearDialog() {
+    document.querySelector('#title').value = "";
+    document.querySelector('#author').value = "";
+    document.querySelector('#pages').value = "";
+    document.querySelector('#haveRead').checked = false;    
+}
+
 const dialog = document.querySelector("dialog");
 const saveButton = document.querySelector("dialog button#save");
 const cancelButton = document.querySelector("dialog button#cancel");
@@ -41,6 +79,8 @@ cancelButton.addEventListener("click", () => {
 saveButton.addEventListener("click", () => {
     dialog.close();
     addBookToLibrary();
+    refreshBookDisplay();
+    clearDialog();
     console.log("Saved\n");
 } );
 
